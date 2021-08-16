3194c88a560bdfcf1479ff6bda15d57b8b347a0d
TomSynGal <80095989+TomSynGal@users.noreply.github.com>
2021-08-16 13:14:29 +0100

    Add files via upload

---
+++ V12_GlobalParameters.py
@@ -0,0 +1,8 @@
+GlobalParams = {'AlphaX': -0.2, 
+                'BetaX': 1.5, 
+                'EpsilonX': 1.0,
+                'PhaseResolution': 48, 
+                'Projections': 24, 
+                'TomographyProjections': 48, 
+                'Samples': 6000,
+                'Nparticles': 1000000}
